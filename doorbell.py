94c6674f5a6370e8b5e2a07ba125290c59464ba8
James Mackley <jtmackley@gmail.com>
2015-05-02 08:47:33 +0100

    Initial code

---
+++ doorbell.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.IN)
+
+#initialise a previous input variable to 0 (assume button not pressed last)
+prev_input = 0
+while True:
+        #take a reading
+        input = GPIO.input(2)
+        #if the last reading was low and this one high, print
+        if ((not prev_input) and input):
+                print (time.strftime("%d/%m/%Y %H:%M:%S")) + " Doorbell!"
+        #update previous input
+        prev_input = input
+        #slight pause to debounce
+        time.sleep(0.05)
+
