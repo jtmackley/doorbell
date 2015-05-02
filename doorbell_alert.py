30cda834b3253314050d1538ad8798607e125164
James Mackley <jtmackley@gmail.com>
2015-05-02 18:16:58 +0100

    Play through speaker support

--- doorbell_alert.py
+++ doorbell_alert.py
@@ -1,9 +1,15 @@
 import doorbell_pushover as pushover
 import time
+import pyglet
 
 def alert():
-        print (time.strftime("%d/%m/%Y %H:%M:%S")) + " Doorbell!"
+	# Output to Local Screen
+	print (time.strftime("%d/%m/%Y %H:%M:%S")) + " Doorbell!"
+	# Play a sound through the speaker
+	bell = pyglet.media.load('bell.wav', streaming=False)
+	bell.play()
+	# Send notification through pushover
 	pushover.send("Doorbell"
 	,"Doorbell rang at: " + (time.strftime("%d/%m/%Y %H:%M:%S")),
-	"http://2seven.co.uk/doorbell")
+	"http://2seven.co.uk/doorbell")	
 	return
