b16756d9665c6740ff2dd36db7bf5c628ce7ab57
James Mackley <jtmackley@gmail.com>
2015-05-03 13:02:28 +0100

    Updated to use absolute paths

--- doorbell_alert.py
+++ doorbell_alert.py
@@ -6,7 +6,7 @@ def alert():
 	# Output to Local Screen
 	print (time.strftime("%d/%m/%Y %H:%M:%S")) + " Doorbell!"
 	# Play a sound through the speaker
-	os.system('mpg123 -q bell.mp3 &')
+	os.system('mpg123 -q /home/pi/doorbell/bell.mp3 &')
 	# Send notification through pushover
 	pushover.send("Doorbell"
 	,"Doorbell rang at: " + (time.strftime("%d/%m/%Y %H:%M:%S")),
