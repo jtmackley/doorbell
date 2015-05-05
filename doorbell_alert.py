fee9c33165a3f24406bc08f3cf7d2eb4237e2957
James Mackley <james@tlc-direct.co.uk>
2015-05-05 14:33:22 +0100

    Further tweaks

--- doorbell_alert.py
+++ doorbell_alert.py
@@ -6,7 +6,8 @@ def alert(cfg):
 	# Output to Local Screen
 	print (time.strftime("%d/%m/%Y %H:%M:%S")) + " " + cfg["message"]
 	# Play a sound through the speaker
-	os.system("mpg123 -q " + cfg["mp3"] + " &")
+	if cfg["localsound"]:
+		os.system("mpg123 -q " + cfg["mp3"] + " &")
 	# Send notification through pushover
 	if cfg["pushover"]:
 		pushover.send(cfg["pushover_app_token"],
@@ -14,5 +15,15 @@ def alert(cfg):
 		cfg["pushover_title"],
 		cfg["message"],
 		cfg["pushover_url"])	
+	# Take a picture
+	img=""
+	if cfg["camera"]:
+		img=takepicture(cfg)
+		# Upload to ftp
+		if cfg["ftp_upload"]:
+			ftp.upload(cfg,img)
+	# Log to mysql
+	if cfg["log_to_mysql"]:
+		mysql.save(cfg,img)
 
 	return
