168e3d30d0d5e786658c119b201b8a1fd0dfa279
James Mackley <jtmackley@gmail.com>
2015-08-22 22:14:51 +0100

    Take picture first

--- doorbell_alert.py
+++ doorbell_alert.py
@@ -4,6 +4,13 @@ import os
 def alert(cfg,action):
 	# Output to Local Screen
 	print (time.strftime("%d/%m/%Y %H:%M:%S")) + " " + cfg["app_message"]
+
+	img=""
+	# Take a picture
+	if (cfg["camera"] & action):
+		import doorbell_camera as camera
+		img=camera.takepicture(cfg)
+		print "Image: " + img
 	# Play a sound through the speaker
 	if (cfg["mp3"] & action):
 		os.system("mpg123 -q " + cfg["mp3file"] + " &")
@@ -14,16 +21,10 @@ def alert(cfg,action):
 	if (cfg["pushbullet"] & action):
 		import doorbell_pushbullet as pbullet
 		pbullet.send(cfg,action)	
-	# Take a picture
-	img=""
-	if (cfg["camera"] & action):
-		import doorbell_camera as camera
-		img=camera.takepicture(cfg)
-		print "Image: " + img
-		# Upload to ftp
-		if (cfg["ftp"] & action):
-			import doorbell_ftp as ftp
-			ftp.upload(cfg,img)
+	# Upload to ftp
+	if (cfg["ftp"] & action):
+		import doorbell_ftp as ftp
+		ftp.upload(cfg,img)
 	# Log to mysql
 	if (cfg["mysql"] & action):
 		import doorbell_mysql as db 
